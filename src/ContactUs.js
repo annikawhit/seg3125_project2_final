432b36fd89e3edc6f310abace07644c37476db39
Annika Whitwam <awhit169@uottawa.ca>
2023-07-02 02:01:57 -0400

    changes to gallery and contact us pages

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -2,6 +2,36 @@ const ContactUs = () => {
     return (  
         <div className="contactus">
             <h1>Contact Us</h1>
+
+            <div className="rowContactUs" id="contact">
+                            
+                <div>
+                    <h4>Hours of Operation</h4>
+                    <table>
+                        <tr><th>Monday</th><td>10am - 7pm</td></tr>
+                        <tr><th>Tuesday</th><td>10am - 7pm</td></tr>
+                        <tr><th>Wednesday</th><td>10am - 7pm</td></tr>
+                        <tr><th>Thursday</th><td>10am - 7pm</td></tr>
+                        <tr><th>Friday</th><td>10am - 9pm</td></tr>
+                        <tr><th>Saturday</th><td>10am - 5pm</td></tr>
+                        <tr><th>Sunday</th><td>10am - 5pm</td></tr>
+                    </table>
+                </div>
+                
+                <div>
+                    <h4>Address</h4>
+                    <table id="address">
+                        <tr><td>75 Laurier Ave E</td></tr>
+                        <tr><td>Ottawa, Ontario</td></tr>
+                        <tr><td>K1N 6N5</td></tr>
+                    </table>
+
+                    <table>
+                        <tr><th>Email:</th><td>contactus@imagineartstudio.ca</td></tr>
+                        <tr><th>Phone:</th><td>613-456-7890</td></tr>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,49 @@
+import image1 from './gallery1.jpeg'
+import image2 from './gallery2.jpeg'
+import image from './home_image_2.jpg'
+import {Link} from 'react-router-dom';
+
 const Gallery = () => {
     return (  
         <div className="gallery">
             <h1>Gallery</h1>
+
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div className="rowGallery" id="gallery">
+                            <div id="image">
+                                <img class="d-block w-100" src={image1} alt="First slide"></img>
+                            </div>
+                            
+                            <div>
+                                <h3 id="text">Student Name: Josephine</h3>
+                                <h3 id="text">Class Name: Watercolours for beginners</h3>
+                                <p id="text">Description: I was able to create this watercolour painting of a bird during my first set of lessons at Imagine Art Studio.</p>
+                                <Link class="btn btn-secondary border-0" id="link" to="/classes">Leave a Comment</Link>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src={image2} alt="Second slide"></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src={image} alt="Third slide"></img>
+                    </div>
+                </div>
+                <a id="gallery_arrow" class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                </a>
+                <a id="gallery_arrow" class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                </a>
+            </div>
         </div>
     );
 }
