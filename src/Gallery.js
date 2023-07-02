f03a71cc54c5f44fd6828aa9838c39f60c27ad4e
Annika Whitwam <awhit169@uottawa.ca>
2023-07-02 17:23:05 -0400

    update gallery page

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,23 +1,19 @@
 import image1 from './gallery1.jpeg'
 import image2 from './gallery2.jpeg'
-import image from './home_image_2.jpg'
+import image3 from './gallery3.jpeg'
 import {Link} from 'react-router-dom';
 import {FaArrowAltCircleRight} from "react-icons/fa"
 import {FaArrowAltCircleLeft} from "react-icons/fa"
+import { CCarousel } from '@coreui/react'
+import { CCarouselItem } from '@coreui/react'
 
 const Gallery = () => {
     return (  
         <div className="gallery">
             <h1>Gallery</h1>
 
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
+            <CCarousel controls>
+                    <CCarouselItem>
                         <div className="rowGallery" id="gallery">
                             <div id="image">
                                 <img class="d-block w-100" src={image1} alt="First slide"></img>
@@ -25,27 +21,41 @@ const Gallery = () => {
                             
                             <div>
                                 <h3 id="text">Student Name: Josephine</h3>
-                                <h3 id="text">Class Name: Watercolours for beginners</h3>
-                                <p id="text">Description: I was able to create this watercolour painting of a bird during my first set of lessons at Imagine Art Studio.</p>
+                                <h3 id="text">Class Name: Watercolours painting for beginners</h3>
+                                <p id="text">Description: I was able to create this watercolour painting of a bird during my first set of lessons at Imagine Art Studio. Thank you to my instructors for all their help.</p>
                                 <Link class="btn btn-secondary border-0" id="link" to="/classes">Send Feedback</Link>
                             </div>
                         </div>
-                        
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src={image2} alt="Second slide"></img>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src={image} alt="Third slide"></img>
-                    </div>
-                </div>
-                <a id="gallery_arrow"  class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <div><FaArrowAltCircleLeft size={35}/></div>
-                </a>
-                <a id="gallery_arrow" class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <div><FaArrowAltCircleRight size={35}/></div>
-                </a>
-            </div>
+                    </CCarouselItem>
+                    <CCarouselItem>
+                    <div className="rowGallery" id="gallery">
+                            <div id="image">
+                                <img class="d-block w-100" src={image2} alt="First slide"></img>
+                            </div>
+                            
+                            <div>
+                                <h3 id="text">Student Name: Helen</h3>
+                                <h3 id="text">Class Name: Pottery for advanced students</h3>
+                                <p id="text">Description: Last week, I took my first advanced class at Imagine Art Studio. During the class, the instructor helped me create this beautiful piece of pottery.</p>
+                                <Link class="btn btn-secondary border-0" id="link" to="/classes">Send Feedback</Link>
+                            </div>
+                        </div>
+                    </CCarouselItem>
+                    <CCarouselItem>
+                    <div className="rowGallery" id="gallery">
+                            <div id="image">
+                                <img class="d-block w-100" src={image3} alt="First slide"></img>
+                            </div>
+                            
+                            <div>
+                                <h3 id="text">Student Name: Leo</h3>
+                                <h3 id="text">Class Name: Drawing for intermediate students</h3>
+                                <p id="text">Description: So far I have taken a few different drawing classes at this art studio and I can already see much improvement in my drawing technique.</p>
+                                <Link class="btn btn-secondary border-0" id="link" to="/classes">Send Feedback</Link>
+                            </div>
+                        </div>
+                    </CCarouselItem>
+            </CCarousel> 
         </div>
     );
 }
